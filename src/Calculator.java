1ac30e0bb2de10df6ba58b16ba00873f78eb5447
NicholasKir <92822987+NicholasKir@users.noreply.github.com>
2021-12-01 10:04:58 +0300

    Calculator2.1

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,7 @@ import java.util.Stack;
  */
 
 public class Calculator {
-    
+
     /**
      * Function that rewrite expression using Reverse Polish notation.
      * @param value - an expression entered by the user
--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,41 @@ public class Main {
         /**
          * Demostration of the work
          */
-        Scanner input = new Scanner(System.in);
-        int check=1;
-        while(check!=0)
+        System.out.println("1.Calculate an expression");
+        System.out.println("0.Exit");
+        Scanner in = new Scanner(System.in);
+        while (!in.hasNextInt()) {
+            System.out.println("Error: enter the number");
+            in.next();
+        }
+        int s = in.nextInt();
+        while (s<0||s>1) {
+            System.out.println("Error: number must be 0 or 1");
+            s = in.nextInt();
+        }
+        while (s != 0)
         {
-            String str;
+
+            Scanner scan = new Scanner(System.in);
             System.out.print("Enter an new expression : ");
-            str = input.nextLine();
+            String str=scan.nextLine();
             String strR;
             double ans = 0;
             strR = Calculator.ToRPN(str);
             ans = Calculator.ToAnswer(strR);
             System.out.println(ans);
-    }
+            System.out.println("1.Calculate an expression");
+            System.out.println("0.Exit");
+            while (!in.hasNextInt()) {
+                System.out.println("Error: enter the number");
+                in.next();
+            }
+            s = in.nextInt();
+            while (s<0||s>1) {
+                System.out.println("Error: number must be 0 or 1");
+                s = in.nextInt();
+            }
+        }
 }
 }
 
